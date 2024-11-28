dba3504039f042db2cb02bc0eb62c2442cc57927
EpicAryan <adaryan0811@gmail.com>
2024-11-28 12:08:00 +0530

    feat: setup custom font and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
     return (
         <main>
-            <div className="text-5xl text-red-500 font-bold">Hello React</div>
+            <div className="text-5xl text-red-primary font-bold"> WELCOME</div>
+        <div className="text-5xl text-yellow-primary font-bold"> WELCOME</div>
+<div className="text-5xl text-yellow-secondary font-bold"> WELCOME</div>
         </main>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ['zentry-regular', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'cicular-web': ['cicular-web'],
+        'robert-medium': ['robert-medium'],
+        'robert-regular': ['robert-regular'],
+      },
+      colors: {
+        blue: {
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        red: {
+          primary: '#ff5246',
+          secondary: '#f03131',
+          tertiary: '#ff1023',
+        },
+        yellow: {
+          primary: "#8e983f",
+          secondary: "#edff66",
+        },
+      }
+    },
   },
   plugins: [],
 }
