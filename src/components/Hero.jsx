3c1e0a0b853875694cf1bc70682c1d18b09a25ff
EpicAryan <adaryan0811@gmail.com>
2024-12-06 03:57:42 +0530

    feat: integrate GSAP for animations in Hero component; add loading indicator and update styles and completing Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,16 @@
 
-import { useState,useRef } from "react";
+import { useState,useRef, useEffect } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
     const [currentIndex,setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
     const totalVideos = 4;
@@ -25,9 +29,68 @@ const Hero = () => {
     };
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
+    useEffect(() => {
+        if(loadedVideos === totalVideos-1) {
+            setIsLoading(false);
+        }
+    },[loadedVideos])
+
+    useGSAP(() =>{
+        if(hasClicked){
+            gsap.set("#next-video",{ visibility: "visible" });
+            gsap.to('#next-video', {
+                transformation: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play(),
+            })
+
+            gsap.from('#current-video', {
+                transformation: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut',
+            })
+        }
+    },
+    {
+        dependencies: [currentIndex],
+        revertOnUpdate: true,
+    });
+
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: "polygon(14% 0, 72% 0, 80% 80%, -2% 80%)",
+            borderRadius: "0% 0% 73% 0%",
+        })
+        gsap.from('#video-frame',{
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0% 0% 0% 0%",
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    });
+
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
-            <div id="video-frame" className="relative z-10 h-dvh overflow-hidden rounded-lg bg-blue-75">
+            {isLoading && (
+                <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-blue-50">
+                    <div className="three-body">
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                    </div>
+                </div>
+            )}
+            <div id="video-frame" className="relative z-10 h-dvh overflow-hidden  bg-blue-75">
                 <div>
                     <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
                         <div 
@@ -55,8 +118,8 @@ const Hero = () => {
                         onLoadedData = {handleVideoLoad}
                     />
                     <video
-                        src={getVideoSrc(currentIndex )}
-                        // autoPlay
+                        src={getVideoSrc( currentIndex === totalVideos - 1 ? 1 : currentIndex )}
+                        autoPlay
                         loop
                         muted
                         className="absolute left-0 top-0 size-full object-cover object-center"
@@ -76,7 +139,7 @@ const Hero = () => {
                             <p className="max-w-68 font-robert-regular text-blue-100 text-sm lg:text-base mb-2">
                                 A 5v5 character-based tactical shooter <br /> with ultimate gaming experience
                             </p>
-                            <Button id="play-now" title="Play Now" leftIcon={<TiLocationArrow/>} containerClass="bg-button-150 flex-center gap-1 text-blue-100"  />
+                            <Button id="play-now" title="Play Now" leftIcon={<TiLocationArrow/>} containerClass="!bg-red-secondary flex-center gap-1"  />
                         </div>
                         
                     </div>
