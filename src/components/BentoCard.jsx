32b01e267ba06f77eab463aacb56abbe5304faa7
EpicAryan <adaryan0811@gmail.com>
2024-12-14 02:35:32 +0530

    feat: add Features and BentoCard components; update App layout and styles

---
+++ src/components/BentoCard.jsx
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import { useRef, useState } from 'react';
+
+const BentoTilt = ({ children, className = ''}) => {
+    const [transformStyle, setTransformStyle] = useState('');
+    const  itemRef = useRef(null);
+
+    const handleMouseMove = (e) => {
+        if(!itemRef.current) return;
+
+        const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (e.clientX - left) / width;
+        const relativeY = (e.clientY - top) / height;
+        
+        const tiltX = (relativeY - 0.5) * 7;
+        const tiltY = (relativeX - 0.5) * -7;
+
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+        setTransformStyle(newTransform);
+    }
+
+    const handleMouseLeave = () => {
+        setTransformStyle('');
+    }
+
+    return (
+        <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}}>
+            {children}
+        </div>
+    )
+}
+
+const BentoCard = ({src, title, description,containerClass}) => {
+  return (
+    <div className={clsx("relative size-full",containerClass)}>
+        <video 
+            src={src}
+            loop
+            muted
+            autoPlay
+            className="absolute left-0 top-0 size-full object-cover object-center rounded-xl"
+        />
+        <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+            <div>
+                <h1 className="bento-title special-font">
+                    {title}
+                </h1>
+                {description && (
+                    <p className="mt-3 max-w-64 text-sm md:text-base">
+                        {description}
+                    </p>
+                )}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export {BentoCard, BentoTilt}
---
+++ src/components/Features.jsx
@@ -0,0 +1,68 @@
+import { TiLocationArrow } from "react-icons/ti"
+import {BentoCard, BentoTilt} from "./BentoCard"
+
+const Features = () => {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="text-blue-50 font-circular-web text-xl">
+            Way to Valorant Arena
+          </p>
+          <p className="max-w-md font-circular-web text-blue-50 text-xl opacity-50">
+            Join over 10 million players worldwide, challenge your friends, and face off in the most challenging battles in Valorant.
+          </p>
+        </div>
+        <BentoTilt className="w-full h-[65vh] md:h-[70vh] border-hsla rounded-xl relative mb-10 overflow-hidden">
+          <BentoCard 
+            src="videos/feature-1.mp4"
+            title = {<>radiant</>}
+            description = "Face off in the most challenging battles in Valorant. See how radiant players defeat their enemies and gain the ultimate reward"
+          />
+        </BentoTilt>
+        <div className="grid h-[140vh] grid-cols-2 grid-rows-3 gap-10">
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2 ">
+            <BentoCard 
+              src="videos/feature-2.mp4"
+              title = {<>challenges</>}
+              description = "Join over 10 million players worldwide, challenge your friends, and face off in the most challenging battles in Valorant."
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 ms-32 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title = {<>community</>}
+              description = "Connect with fellow Valorant players, share your experiences, and learn from each other. Join the Valorant community today!"
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 me-24 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title = {<>Axule</>}
+              description = "Connect with fellow Valorant players, share your experiences, and learn from each other. Join the Valorant community today!"
+            />
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-red-secondary p-5">
+              <h1 className="bento-title max-w-64 text-black">Additional sections launching soon</h1>
+              <TiLocationArrow className="m-5 scale-[5] self-end"/>
+            </div>
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
+          </BentoTilt>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Features
